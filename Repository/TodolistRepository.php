80e79c627c74b7e7a6b53118479882bede6fdcd9
adam <ach.adam04@gmail.com>
2024-09-15 12:07:32 +0700

    Test Repository dan Service : Menampilkan Todolist

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -11,4 +11,24 @@ namespace Repository {
         function remove(int $number): bool;
         function findAll(): array;
     }
+
+    class TodolistRepositoryImpl implements TodolistRepository {
+
+        public array $todolist = array();
+
+        function save(Todolist $todolist): void
+        {
+            //
+        }
+
+        function remove(int $number): bool
+        {
+            //
+        }
+
+        function findAll(): array
+        {
+            return $this->todolist;
+        }
+    }
 }
