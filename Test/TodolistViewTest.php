8a2aa84f04a000bf40698d9a785929765a39752f
adam <ach.adam04@gmail.com>
2024-09-16 06:16:13 +0700

    Test View : Menampilkan Todolist

---
+++ Test/TodolistViewTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../Entity/Todolist.php';
+require_once __DIR__ . '/../Helper/InputHelper.php';
+require_once __DIR__ . '/../Repository/TodolistRepository.php';
+require_once __DIR__ . '/../Service/TodolistService.php';
+require_once __DIR__ . '/../View/TodolistView.php';
+
+use Repository\TodolistRepositoryImpl;
+use Service\TodolistServiceImpl;
+use View\TodolistView;
+
+function TestViewShowTodolist(): void 
+{
+    $todolistRepository = new TodolistRepositoryImpl();
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+    $todolistView = new TodolistView($todolistService);
+
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+    $todolistService->addTodolist("Belajar PHP Database");
+
+    $todolistView->showTodoList();
+}
+
+TestViewShowTodolist();
+
+?>
