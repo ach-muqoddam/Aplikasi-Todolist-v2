0a78ac4d59416fdaa6e8f4106c5c17c45acb54c9
adam <ach.adam04@gmail.com>
2024-09-16 08:13:14 +0700

    View : Menghapus Todolist

--- View/TodolistView.php
+++ View/TodolistView.php
@@ -56,6 +56,25 @@ namespace View {
         function removeTodolist(): void 
         {
             
+            echo "MENGHAPUS TODO" . PHP_EOL;
+
+            $pilihan = InputHelper::input("Nomor (x untuk membatalkan)");
+
+            // cek jika ingin batalkan
+            if ($pilihan == "x") {
+                echo "batal menghapus todo" . PHP_EOL;
+            } else {   
+                // cek berhasil atau gagal saat dihapus
+                $success = $this->todolistService->removeTodoList($pilihan);
+
+                // cek kondisi untuk melakukan penghapusan
+                if ($success) {
+                    echo "sukses menghapus todo nomor $pilihan" . PHP_EOL;
+                } else {
+                    echo "nomer $pilihan tidak ada di list todo" . PHP_EOL;
+                }
+            }
+
         }
     }
 
